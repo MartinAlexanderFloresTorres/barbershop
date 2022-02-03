3acf757ea21111a1a6b6f971db1be3b0e8a8e019
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-02-03 13:22:34 -0500

    Primer commit master de barbershop

---
+++ js/scrollreveal.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.banner_btn', {Delay: 500});
+ScrollReveal().reveal('.facture__conten', {Delay: 500});
+ScrollReveal().reveal('.imagen', {Delay: 500});
+ScrollReveal().reveal('.servicios__imagen', {Delay: 500});
+ScrollReveal().reveal('.user', {Delay: 500});
+ScrollReveal().reveal('.footer', {Delay: 500});
